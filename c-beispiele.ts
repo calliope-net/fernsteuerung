77905d9a562c74a788a30962129798a20a1ddb16
calliope-net <141074597+calliope-net@users.noreply.github.com>
2024-07-14 18:07:21 +0200

    Aktualisiere c-beispiele.ts

--- c-beispiele.ts
+++ c-beispiele.ts
@@ -50,14 +50,24 @@ namespace cb2 { // c-beispiele.ts
 
             if (readSpursensor(eDH.dunkel, eDH.dunkel, eI2C.x21)) {
                 writeMotor128Servo16(motor128, 16) // nicht lenken
+
             } else if (readSpursensor(eDH.dunkel, eDH.hell)) { // 0% Rad steht bei voller Lenkung (1 oder 31)
                 writeMotor128Servo16(langsamfahren, 16 - lenken, 0) // rechts lenken >16 = 31
+                m_lenken = 16 + lenken
+
+            } else if (readSpursensor(eDH.hell, eDH.dunkel)) { // 0% Rad steht bei voller Lenkung (1 oder 31)
+                writeMotor128Servo16(langsamfahren, 16 + lenken, 0) // rechts lenken >16 = 31
+                m_lenken = 16 - lenken
+
             } else {
-                writeMotor128Servo16(langsamfahren, 16 + lenken, 0) // links lenken <16 = 1
+
+                writeMotor128Servo16(langsamfahren, m_lenken, 0) // entgegen lenken
             }
             return true
         }
     }
+
+    let m_lenken = 16
 
 
     // blockId=cb2_speedPicker block="%speed" blockHidden=true
