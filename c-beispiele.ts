457306f56f70635ecdadb8a2f7d5c2f0a3f278a5
calliope-net <141074597+calliope-net@users.noreply.github.com>
2024-07-14 18:54:53 +0200

    Aktualisiere c-beispiele.ts

--- c-beispiele.ts
+++ c-beispiele.ts
@@ -44,6 +44,8 @@ namespace cb2 { // c-beispiele.ts
             writeMotorenStop()
             return false
         } else {
+            writeLed(eLed.redl, false)
+            writeLed(eLed.redr, false)
 
             let langsamfahren = btf.motorProzent(motor128, 50)
             let lenken = Math.abs(servo16 - 16)  // 16-16=0 / 1-16=15 / 31-16=15
@@ -54,11 +56,13 @@ namespace cb2 { // c-beispiele.ts
             } else if (readSpursensor(eDH.dunkel, eDH.hell)) { // 0% Rad steht bei voller Lenkung (1 oder 31)
                 writeMotor128Servo16(langsamfahren, 16 - lenken, 0) // links lenken <16 = 1
                 m_lenken = 16 + lenken
+                writeLed(eLed.redr, true)
 
             }
             else if (readSpursensor(eDH.hell, eDH.dunkel)) { // 0% Rad steht bei voller Lenkung (1 oder 31)
                 writeMotor128Servo16(langsamfahren, 16 + lenken, 0) // rechts lenken >16 = 31
                 m_lenken = 16 - lenken
+                writeLed(eLed.redl, true)
             }
             else if (m_lenken) {
 
@@ -67,6 +71,8 @@ namespace cb2 { // c-beispiele.ts
 
             else {
                 writeMotor128Servo16(langsamfahren, 16 + lenken, 0) // rechts lenken >16 = 31
+                writeLed(eLed.redl, true)
+                writeLed(eLed.redr, true)
             }
 
             return true
