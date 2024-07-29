4459de2e66853901d5e683958f7b4cfc40ccf100
calliope-net <141074597+calliope-net@users.noreply.github.com>
2024-07-29 12:22:25 +0200

    Aktualisiere main.blocks, r-fernsteuerung.ts

--- r-fernsteuerung.ts
+++ r-fernsteuerung.ts
@@ -1,21 +1,24 @@
 
 namespace receiver { // r-fernsteuerung.ts
 
-    //% group="Betriebsart" subcategory="Fernsteuerung"
-    //% block="%receivedData 00 Fernsteuerung Motoren" weight=5
-    //% receivedData.shadow="btf_receivedBuffer19"
-    export function sendM0(receivedData: Buffer) {
+    //% group="00 Fernsteuerung mit Joystick (reagiert auf Sensoren)" subcategory="Fernsteuerung"
+    //% block="Fahren und Lenken mit Joystick aus Datenpaket %buffer M:01ABCD S:0" weight=5
+    //% buffer.shadow="btf_receivedBuffer19"
+    export function sendM0(buffer: Buffer) {
 
-        if (btf.isBetriebsart(receivedData, btf.e0Betriebsart.p0Fahren)) {
-            receiver.dualMotor128(receiver.eDualMotor.M0, btf.getByte(receivedData, btf.eBufferPointer.m0, btf.eBufferOffset.b0_Motor))
-            receiver.pinServo16(btf.getByte(receivedData, btf.eBufferPointer.m0, btf.eBufferOffset.b1_Servo))
-            receiver.dualMotor128(receiver.eDualMotor.M1, btf.getByte(receivedData, btf.eBufferPointer.m1, btf.eBufferOffset.b0_Motor))
-            receiver.qwiicMotorChipPower(receiver.eQwiicMotorChip.ab, btf.getaktiviert(receivedData, btf.e3aktiviert.ma) || btf.getaktiviert(receivedData, btf.e3aktiviert.mb))
-            receiver.qwiicMotor128(receiver.eQwiicMotor.ma, btf.getByte(receivedData, btf.eBufferPointer.ma, btf.eBufferOffset.b0_Motor))
-            receiver.qwiicMotor128(receiver.eQwiicMotor.mb, btf.getByte(receivedData, btf.eBufferPointer.mb, btf.eBufferOffset.b0_Motor))
-            receiver.qwiicMotorChipPower(receiver.eQwiicMotorChip.cd, btf.getaktiviert(receivedData, btf.e3aktiviert.mc))
-            receiver.qwiicMotor128(receiver.eQwiicMotor.mc, btf.getByte(receivedData, btf.eBufferPointer.mc, btf.eBufferOffset.b0_Motor))
-            receiver.qwiicMotor128(receiver.eQwiicMotor.md, btf.getByte(receivedData, btf.eBufferPointer.md, btf.eBufferOffset.b0_Motor))
+        if (btf.isBetriebsart(buffer, btf.e0Betriebsart.p0Fahren)) {
+            // Motor M0+Servo M1
+            receiver.dualMotor128(receiver.eDualMotor.M0, btf.getByte(buffer, btf.eBufferPointer.m0, btf.eBufferOffset.b0_Motor))
+            receiver.pinServo16(btf.getByte(buffer, btf.eBufferPointer.m0, btf.eBufferOffset.b1_Servo))
+            receiver.dualMotor128(receiver.eDualMotor.M1, btf.getByte(buffer, btf.eBufferPointer.m1, btf.eBufferOffset.b0_Motor))
+            // Qwiic Motor A B
+            receiver.qwiicMotorChipPower(receiver.eQwiicMotorChip.ab, btf.getaktiviert(buffer, btf.e3aktiviert.ma) || btf.getaktiviert(buffer, btf.e3aktiviert.mb))
+            receiver.qwiicMotor128(receiver.eQwiicMotor.ma, btf.getByte(buffer, btf.eBufferPointer.ma, btf.eBufferOffset.b0_Motor))
+            receiver.qwiicMotor128(receiver.eQwiicMotor.mb, btf.getByte(buffer, btf.eBufferPointer.mb, btf.eBufferOffset.b0_Motor))
+            // Qwiic Motor C D
+            receiver.qwiicMotorChipPower(receiver.eQwiicMotorChip.cd, btf.getaktiviert(buffer, btf.e3aktiviert.mc))
+            receiver.qwiicMotor128(receiver.eQwiicMotor.mc, btf.getByte(buffer, btf.eBufferPointer.mc, btf.eBufferOffset.b0_Motor))
+            receiver.qwiicMotor128(receiver.eQwiicMotor.md, btf.getByte(buffer, btf.eBufferPointer.md, btf.eBufferOffset.b0_Motor))
 
             //receiver.ringTone(btf.getSchalter(receivedData, btf.e0Schalter.b0))
             //receiver.qwiicRelay(btf.getSchalter(receivedData, btf.e0Schalter.b1))
