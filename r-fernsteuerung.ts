0678fbdf65cedf566927f3d0851970cc30ab7a8d
calliope-net <141074597+calliope-net@users.noreply.github.com>
2024-08-04 18:03:59 +0200

    Aktualisiere r-fernsteuerung.ts

--- r-fernsteuerung.ts
+++ r-fernsteuerung.ts
@@ -11,8 +11,6 @@ namespace receiver { // r-fernsteuerung.ts
 
         if (btf.isBetriebsart(buffer, btf.e0Betriebsart.p0Fahren)) {
 
-            // readQwiicUltrasonic()
-
             // Motor M0+Servo M1 (Fahren und Lenken)
             if (btf.getaktiviert(buffer, btf.e3aktiviert.m0)) {
 
@@ -21,75 +19,42 @@ namespace receiver { // r-fernsteuerung.ts
                 let cmAbstandSensor = 0
                 let bSpur = btf.getSensor(buffer, btf.eBufferPointer.m0, btf.eSensor.b5Spur)
 
+                // nur LEDs schalten und Abstandssensor lesen
                 if (bAbstand) {
                     setLedColors(eRGBled.b, Colors.Yellow, bAbstand) // nicht blinken, bringt I²C Sensor durcheinender
                     setLedColors(eRGBled.c, Colors.White, bSpur)
                     bRichtung_vor = btf.getByte(buffer, btf.eBufferPointer.m0, btf.eBufferOffset.b0_Motor) > c_MotorStop // Fahrtrichtung vorwärts
                     cmAbstandSensor = selectAbstand(true) // immer messen, auch bei Stop, damit der kleiner werdende Wert erkannt wird
-                } else if (bSpur) {
+                }
+                else if (bSpur) {
                     setLedColors(eRGBled.b, Colors.White, pinSpurlinks(eDH.hell))
                     setLedColors(eRGBled.c, Colors.White, pinSpurrechts(eDH.hell))
-                } else { // Spur auch anzeigen, wenn Sensor nicht aktiv
+                }
+                else { // Spur auch anzeigen, wenn Sensor nicht aktiv (dunkelweiß)
                     setLedColors(eRGBled.b, 0x404040, pinSpurlinks(eDH.hell))
                     setLedColors(eRGBled.c, 0x404040, pinSpurrechts(eDH.hell))
                 }
 
-
-                if (bAbstand && bRichtung_vor && (cmAbstandSensor <= btf.getAbstand(buffer))) {
+                // Abstandssensor auswerten
+                if (bAbstand && bRichtung_vor && (cmAbstandSensor <= btf.getAbstand(buffer)))
                     n_AbstandStop = true
-                } else if (!bAbstand || !bRichtung_vor)
+                else if (!bAbstand || !bRichtung_vor)
                     n_AbstandStop = false
 
-                if (bSpur && (pinSpurlinks(eDH.dunkel) || pinSpurrechts(eDH.dunkel))) {
+                // Spursensor auswerten
+                if (bSpur && (pinSpurlinks(eDH.dunkel) || pinSpurrechts(eDH.dunkel)))
                     n_SpurStop = true
-                } else if (!bSpur)
+                else if (!bSpur)
                     n_SpurStop = false
 
                 if (!n_AbstandStop && !n_SpurStop) {
                     // Motor M0+Servo M1 (Fahren und Lenken)
-                    // dualMotor128(receiver.eDualMotor.M0, btf.getByte(buffer, btf.eBufferPointer.m0, btf.eBufferOffset.b0_Motor))
                     selectMotor(btf.getByte(buffer, btf.eBufferPointer.m0, btf.eBufferOffset.b0_Motor))
                     pinServo16(btf.getByte(buffer, btf.eBufferPointer.m0, btf.eBufferOffset.b1_Servo))
-
-                } else {
-                    // dualMotor128(eDualMotor.M0, c_DualMotorStop)
+                }
+                else {
                     selectMotor(c_MotorStop)
                 }
-
-
-
-
-                /*  if (btf.getSensor(buffer, btf.eBufferPointer.m0, btf.eSensor.b6Abstand) // Abstandssensor aktiviert
-                     &&
-                     selectAbstandSensorConnected()
-                     &&
-                     btf.getByte(buffer, btf.eBufferPointer.m0, btf.eBufferOffset.b0_Motor) > c_DualMotorStop // Fahrtrichtung vorwärts
-                     &&
-                     selectAbstand(true) < btf.getAbstand(buffer)) { // Abstand messen
- 
-                     dualMotor128(eDualMotor.M0, c_DualMotorStop) //  writeMotorenStop()
- 
-                     setLedColors(eRGBled.b, Colors.Red, true)
-                     //rgbLEDs(eRGBled.b, Colors.Red, true)
-                     // writeRgbLed(eRgbLed.lh, Colors.Red, true, true)
-                 }
-                 else if (btf.getSensor(buffer, btf.eBufferPointer.m0, btf.eSensor.b5Spur) // Spursensor aktiviert
-                     &&
-                     (pinSpurlinks(eDH.dunkel) || pinSpurrechts(eDH.dunkel))) { // schwarze Linie erkannt / nicht hell, hell
- 
-                     dualMotor128(eDualMotor.M0, c_DualMotorStop) //  writeMotorenStop()
- 
-                     setLedColors(eRGBled.b, Colors.White, true)
-                     //writeRgbLed(eRgbLed.rh, Colors.White, true, true)
-                 }
-                 else {
-                     // Motor M0+Servo M1 (Fahren und Lenken)
-                     receiver.dualMotor128(receiver.eDualMotor.M0, btf.getByte(buffer, btf.eBufferPointer.m0, btf.eBufferOffset.b0_Motor))
-                     receiver.pinServo16(btf.getByte(buffer, btf.eBufferPointer.m0, btf.eBufferOffset.b1_Servo))
- 
-                     setLedColors(eRGBled.b, Colors.Red, false)
-                 }
-                  */
             }
 
 
@@ -98,16 +63,24 @@ namespace receiver { // r-fernsteuerung.ts
                 receiver.dualMotor128(receiver.eDualMotor.M1, btf.getByte(buffer, btf.eBufferPointer.m1, btf.eBufferOffset.b0_Motor))
             }
             // Qwiic Motor A B
-            receiver.qwiicMotorChipPower(receiver.eQwiicMotorChip.ab, btf.getaktiviert(buffer, btf.e3aktiviert.ma) || btf.getaktiviert(buffer, btf.e3aktiviert.mb))
-            receiver.qwiicMotor128(receiver.eQwiicMotor.ma, btf.getByte(buffer, btf.eBufferPointer.ma, btf.eBufferOffset.b0_Motor))
-            receiver.qwiicMotor128(receiver.eQwiicMotor.mb, btf.getByte(buffer, btf.eBufferPointer.mb, btf.eBufferOffset.b0_Motor))
+            if (btf.getaktiviert(buffer, btf.e3aktiviert.ma)) {
+                receiver.qwiicMotorChipPower(receiver.eQwiicMotorChip.ab, true)
+                receiver.qwiicMotor128(receiver.eQwiicMotor.ma, btf.getByte(buffer, btf.eBufferPointer.ma, btf.eBufferOffset.b0_Motor))
+            }
+            if (btf.getaktiviert(buffer, btf.e3aktiviert.mb)) {
+                receiver.qwiicMotorChipPower(receiver.eQwiicMotorChip.ab, true)
+                receiver.qwiicMotor128(receiver.eQwiicMotor.mb, btf.getByte(buffer, btf.eBufferPointer.mb, btf.eBufferOffset.b0_Motor))
+            }
             // Qwiic Motor C D
-            receiver.qwiicMotorChipPower(receiver.eQwiicMotorChip.cd, btf.getaktiviert(buffer, btf.e3aktiviert.mc) || btf.getaktiviert(buffer, btf.e3aktiviert.md))
-            receiver.qwiicMotor128(receiver.eQwiicMotor.mc, btf.getByte(buffer, btf.eBufferPointer.mc, btf.eBufferOffset.b0_Motor))
-            receiver.qwiicMotor128(receiver.eQwiicMotor.md, btf.getByte(buffer, btf.eBufferPointer.md, btf.eBufferOffset.b0_Motor))
-
+            if (btf.getaktiviert(buffer, btf.e3aktiviert.mc)) {
+                receiver.qwiicMotorChipPower(receiver.eQwiicMotorChip.cd, true)
+                receiver.qwiicMotor128(receiver.eQwiicMotor.mc, btf.getByte(buffer, btf.eBufferPointer.mc, btf.eBufferOffset.b0_Motor))
+            }
+            if (btf.getaktiviert(buffer, btf.e3aktiviert.md)) {
+                receiver.qwiicMotorChipPower(receiver.eQwiicMotorChip.cd, true)
+                receiver.qwiicMotor128(receiver.eQwiicMotor.md, btf.getByte(buffer, btf.eBufferPointer.md, btf.eBufferOffset.b0_Motor))
+            }
         }
-
     }
 
 
