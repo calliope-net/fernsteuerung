5fd615237c68e9e43c09f30684abc773a2a3d190
calliope-net <141074597+calliope-net@users.noreply.github.com>
2024-08-08 16:40:24 +0200

    Aktualisiere main.blocks, r-fernsteuerung.ts, c-fernsteuerung.ts

--- c-fernsteuerung.ts
+++ c-fernsteuerung.ts
@@ -5,42 +5,46 @@ namespace cb2 { // c-fernsteuerung.ts
     // ========== group="Fernsteuerung mit Joystick (reagiert auf Sensoren)" subcategory="Fernsteuerung"
 
     //% group="00 Fernsteuerung mit Joystick (reagiert auf Sensoren)" subcategory="Fernsteuerung"
-    //% block="Fahren und Lenken mit Joystick aus Datenpaket %buffer lenken %lenkenProzent \\%" weight=8
+    //% block="00 Fahren und Lenken mit Joystick (MS:0) aus %buffer • lenken %lenkenProzent \\%" weight=8
     //% buffer.shadow=btf_receivedBuffer19
     //% lenkenProzent.min=10 lenkenProzent.max=90 lenkenProzent.defl=50
     export function fahreJoystick(buffer: Buffer, lenkenProzent = 50) {
-        let iBufferPointer = btf.eBufferPointer.m0
 
-        if (btf.getSensor(buffer, iBufferPointer, btf.eSensor.b6Abstand) // Abstandssensor aktiviert
-            &&
-            btf.getByte(buffer, iBufferPointer, btf.eBufferOffset.b0_Motor) > 128 // Fahrtrichtung vorwärts
-            &&
-            readUltraschallAbstand() < btf.getAbstand(buffer)) { // Abstand messen
+        if (btf.isBetriebsart(buffer, btf.e0Betriebsart.p0Fahren) && btf.getaktiviert(buffer, btf.e3aktiviert.m0)) { // Betriebsart 00 mit Joystick fernsteuern
 
-            writeMotorenStop()
+            let iBufferPointer = btf.eBufferPointer.m0
 
-            writeRgbLed(eRgbLed.lh, Colors.Red, true, true)
-        }
-        else if (btf.getSensor(buffer, iBufferPointer, btf.eSensor.b5Spur) // Spursensor aktiviert
-            &&
-            !readSpursensor(eDH.hell, eDH.hell, true)) { // schwarze Linie erkannt / nicht hell, hell
+            if (btf.getSensor(buffer, iBufferPointer, btf.eSensor.b6Abstand) // Abstandssensor aktiviert
+                &&
+                btf.getByte(buffer, iBufferPointer, btf.eBufferOffset.b0_Motor) > 128 // Fahrtrichtung vorwärts
+                &&
+                readUltraschallAbstand() < btf.getAbstand(buffer)) { // Abstand messen
 
-            writeMotorenStop()
+                writeMotorenStop()
 
-            writeRgbLed(eRgbLed.rh, Colors.White, true, true)
-        }
-        // Stoßstange noch abfragen
-        else {
+                writeRgbLed(eRgbLed.lh, Colors.Red, true, true)
+            }
+            else if (btf.getSensor(buffer, iBufferPointer, btf.eSensor.b5Spur) // Spursensor aktiviert
+                &&
+                !readSpursensor(eDH.hell, eDH.hell, true)) { // schwarze Linie erkannt / nicht hell, hell
 
-            writeMotor128Servo16(
-                btf.getByte(buffer, iBufferPointer, btf.eBufferOffset.b0_Motor),
-                btf.getByte(buffer, iBufferPointer, btf.eBufferOffset.b1_Servo),
-                lenkenProzent
-            )
+                writeMotorenStop()
 
-            writeRgbLed(eRgbLed.lh, Colors.Yellow, btf.getSensor(buffer, iBufferPointer, btf.eSensor.b6Abstand))
+                writeRgbLed(eRgbLed.rh, Colors.White, true, true)
+            }
+            // Stoßstange noch abfragen
+            else {
 
-            writeRgbLed(eRgbLed.rh, Colors.White, btf.getSensor(buffer, iBufferPointer, btf.eSensor.b5Spur))
+                writeMotor128Servo16(
+                    btf.getByte(buffer, iBufferPointer, btf.eBufferOffset.b0_Motor),
+                    btf.getByte(buffer, iBufferPointer, btf.eBufferOffset.b1_Servo),
+                    lenkenProzent
+                )
+
+                writeRgbLed(eRgbLed.lh, Colors.Yellow, btf.getSensor(buffer, iBufferPointer, btf.eSensor.b6Abstand))
+
+                writeRgbLed(eRgbLed.rh, Colors.White, btf.getSensor(buffer, iBufferPointer, btf.eSensor.b5Spur))
+            }
         }
     }
 
@@ -49,40 +53,49 @@ namespace cb2 { // c-fernsteuerung.ts
     // ========== group="Fernsteuerung 2 Motoren (reagiert auf Sensoren)" subcategory="Fernsteuerung"
 
     //% group="00 Fernsteuerung 2 Motoren (reagiert auf Sensoren)" subcategory="Fernsteuerung"
-    //% block="2 Motoren (AB) steuern aus Datenpaket %buffer" weight=8
+    //% block="00 Fahren 2 Motoren (M:AB) aus %buffer" weight=8
     //% buffer.shadow=btf_receivedBuffer19
     export function fahre2Motoren(buffer: Buffer) {
-        let iBufferPointerA = btf.eBufferPointer.ma
 
-        if (btf.getSensor(buffer, iBufferPointerA, btf.eSensor.b6Abstand) // Abstandssensor aktiviert
+        if (btf.isBetriebsart(buffer, btf.e0Betriebsart.p0Fahren) // Betriebsart 00 mit Joystick fernsteuern
             &&
-            btf.getByte(buffer, iBufferPointerA, btf.eBufferOffset.b0_Motor) > 128 // Fahrtrichtung vorwärts
+            btf.getaktiviert(buffer, btf.e3aktiviert.ma)
             &&
-            readUltraschallAbstand() < btf.getAbstand(buffer)) { // Abstand messen
+            btf.getaktiviert(buffer, btf.e3aktiviert.mb)
+        ) {
 
-            writeMotorenStop()
+            let iBufferPointerA = btf.eBufferPointer.ma
 
-            writeRgbLed(eRgbLed.lh, Colors.Red, true, true)
-        }
-        else if (btf.getSensor(buffer, iBufferPointerA, btf.eSensor.b5Spur) // Spursensor aktiviert
-            &&
-            !readSpursensor(eDH.hell, eDH.hell, true)) { // schwarze Linie erkannt / nicht hell, hell
+            if (btf.getSensor(buffer, iBufferPointerA, btf.eSensor.b6Abstand) // Abstandssensor aktiviert
+                &&
+                btf.getByte(buffer, iBufferPointerA, btf.eBufferOffset.b0_Motor) > 128 // Fahrtrichtung vorwärts
+                &&
+                readUltraschallAbstand() < btf.getAbstand(buffer)) { // Abstand messen
 
-            writeMotorenStop()
+                writeMotorenStop()
 
-            writeRgbLed(eRgbLed.rh, Colors.White, true, true)
-        }
-        // Stoßstange noch abfragen
-        else {
+                writeRgbLed(eRgbLed.lh, Colors.Red, true, true)
+            }
+            else if (btf.getSensor(buffer, iBufferPointerA, btf.eSensor.b5Spur) // Spursensor aktiviert
+                &&
+                !readSpursensor(eDH.hell, eDH.hell, true)) { // schwarze Linie erkannt / nicht hell, hell
 
-            writeMotoren128(
-                btf.getByte(buffer, btf.eBufferPointer.ma, btf.eBufferOffset.b0_Motor),
-                btf.getByte(buffer, btf.eBufferPointer.mb, btf.eBufferOffset.b0_Motor)
-            )
+                writeMotorenStop()
 
-            writeRgbLed(eRgbLed.lh, Colors.Yellow, btf.getSensor(buffer, iBufferPointerA, btf.eSensor.b6Abstand))
+                writeRgbLed(eRgbLed.rh, Colors.White, true, true)
+            }
+            // Stoßstange noch abfragen
+            else {
 
-            writeRgbLed(eRgbLed.rh, Colors.White, btf.getSensor(buffer, iBufferPointerA, btf.eSensor.b5Spur))
+                writeMotoren128(
+                    btf.getByte(buffer, btf.eBufferPointer.ma, btf.eBufferOffset.b0_Motor),
+                    btf.getByte(buffer, btf.eBufferPointer.mb, btf.eBufferOffset.b0_Motor)
+                )
+
+                writeRgbLed(eRgbLed.lh, Colors.Yellow, btf.getSensor(buffer, iBufferPointerA, btf.eSensor.b6Abstand))
+
+                writeRgbLed(eRgbLed.rh, Colors.White, btf.getSensor(buffer, iBufferPointerA, btf.eSensor.b5Spur))
+            }
         }
     }
 
@@ -95,46 +108,48 @@ namespace cb2 { // c-fernsteuerung.ts
     let n_fahrplanBuffer5Strecken_gestartet = false
 
     //% group="20 Fahrplan (5 Teilstrecken) empfangen" subcategory="Fernsteuerung"
-    //% block="fahre Strecke 1-5 aus Datenpaket %buffer Start Bit %startBit" weight=4
+    //% block="20 Fahren Strecke 1-5 (MS:1ABCD) aus %buffer • Start Bit %startBit" weight=4
     //% buffer.shadow=btf_receivedBuffer19
     //% startBit.defl=btf.e3aktiviert.m1
     export function fahrplanBuffer5Strecken(buffer: Buffer, startBit: btf.e3aktiviert) {
 
-        if (!n_fahrplanBuffer5Strecken_gestartet && btf.getaktiviert(buffer, startBit)) { // m1 true
-            n_fahrplanBuffer5Strecken_gestartet = true
-            btf.zeigeBIN(0, btf.ePlot.bin, 2)
+        if (btf.isBetriebsart(buffer, btf.e0Betriebsart.p2Fahrplan)) { // Betriebsart 20 Fahrplan senden
 
-            let i = btf.getByte(buffer, btf.eBufferPointer.m0, btf.eBufferOffset.b1_Servo) // Anzahl Durchläufe gesamt
-            if (i == 0)
-                i = 1 // 0=1x 1=1x 2=2x 3=3x ...
+            if (!n_fahrplanBuffer5Strecken_gestartet && btf.getaktiviert(buffer, startBit)) { // m1 true
+                n_fahrplanBuffer5Strecken_gestartet = true
+                btf.zeigeBIN(0, btf.ePlot.bin, 2)
 
-            for (i; i > 0; i--) {
+                let i = btf.getByte(buffer, btf.eBufferPointer.m0, btf.eBufferOffset.b1_Servo) // Anzahl Durchläufe gesamt
+                if (i == 0)
+                    i = 1 // 0=1x 1=1x 2=2x 3=3x ...
 
-                for (let iBufferPointer = btf.eBufferPointer.m1; iBufferPointer < 19; iBufferPointer += 3) { // 4, 7, 10, 13, 16
+                for (i; i > 0; i--) {
 
-                    btf.zeigeBINx234Fahrplan5Strecken(buffer, iBufferPointer) // anzeigen im 5x5 Display
+                    for (let iBufferPointer = btf.eBufferPointer.m1; iBufferPointer < 19; iBufferPointer += 3) { // 4, 7, 10, 13, 16
 
-                    // fahreStrecke testet Gültigkeit der Parameter
-                    // fahreStrecke wertet auch Encoder, Abstand- und Spur- Sensoren aus
-                    fahreStrecke(
-                        btf.getByte(buffer, iBufferPointer, btf.eBufferOffset.b0_Motor),
-                        btf.getByte(buffer, iBufferPointer, btf.eBufferOffset.b1_Servo),
-                        btf.getByte(buffer, iBufferPointer, btf.eBufferOffset.b2_Fahrstrecke),
-                        btf.getSensor(buffer, iBufferPointer, btf.eSensor.b6Abstand),
-                        btf.getAbstand(buffer),
-                        btf.getSensor(buffer, iBufferPointer, btf.eSensor.b5Spur),
-                        btf.getSensor(buffer, iBufferPointer, btf.eSensor.b7Impulse)
-                    )
-                } // for iBufferPointer
+                        btf.zeigeBINx234Fahrplan5Strecken(buffer, iBufferPointer) // anzeigen im 5x5 Display
+
+                        // fahreStrecke testet Gültigkeit der Parameter
+                        // fahreStrecke wertet auch Encoder, Abstand- und Spur- Sensoren aus
+                        fahreStrecke(
+                            btf.getByte(buffer, iBufferPointer, btf.eBufferOffset.b0_Motor),
+                            btf.getByte(buffer, iBufferPointer, btf.eBufferOffset.b1_Servo),
+                            btf.getByte(buffer, iBufferPointer, btf.eBufferOffset.b2_Fahrstrecke),
+                            btf.getSensor(buffer, iBufferPointer, btf.eSensor.b6Abstand),
+                            btf.getAbstand(buffer),
+                            btf.getSensor(buffer, iBufferPointer, btf.eSensor.b5Spur),
+                            btf.getSensor(buffer, iBufferPointer, btf.eSensor.b7Impulse)
+                        )
+                    } // for iBufferPointer
+                }
+            }
+            else if (n_fahrplanBuffer5Strecken_gestartet && !btf.getaktiviert(buffer, startBit)) { // m1 false
+                n_fahrplanBuffer5Strecken_gestartet = false
+                btf.zeigeBIN(0, btf.ePlot.bin, 2)
+                btf.zeigeBIN(0, btf.ePlot.bin, 3)
+                btf.zeigeBIN(0, btf.ePlot.bin, 4)
             }
         }
-        else if (n_fahrplanBuffer5Strecken_gestartet && !btf.getaktiviert(buffer, startBit)) { // m1 false
-            n_fahrplanBuffer5Strecken_gestartet = false
-            btf.zeigeBIN(0, btf.ePlot.bin, 2)
-            btf.zeigeBIN(0, btf.ePlot.bin, 3)
-            btf.zeigeBIN(0, btf.ePlot.bin, 4)
-        }
-
     }
 
 
@@ -144,72 +159,74 @@ namespace cb2 { // c-fernsteuerung.ts
     let n_fahrplanBuffer2x2Motoren_gestartet = false
 
     //% group="20 Fahrplan (2 Teilstrecken • 2 Motoren) empfangen" subcategory="Fernsteuerung"
-    //% block="fahre 2 Strecken mit 2 Motoren aus Datenpaket %buffer Start Bit %startBit || Encoder %checkEncoder" weight=4
+    //% block="20 Fahren 2 Strecken mit 2 Motoren (MS:AB CD) aus %buffer • Start Bit %startBit || • Encoder %checkEncoder" weight=4
     //% buffer.shadow=btf_receivedBuffer19
     //% startBit.defl=btf.e3aktiviert.ma
     //% checkEncoder.shadow=toggleYesNo checkEncoder.defl=1
     //% inlineInputMode=inline
     export function fahrplanBuffer2x2Motoren(buffer: Buffer, startBit: btf.e3aktiviert, checkEncoder = true) {
 
-        if (!n_fahrplanBuffer2x2Motoren_gestartet && btf.getaktiviert(buffer, startBit)) { // ma true
-            n_fahrplanBuffer2x2Motoren_gestartet = true
-            btf.zeigeBIN(0, btf.ePlot.bin, 2) // x=2 löschen
+        if (btf.isBetriebsart(buffer, btf.e0Betriebsart.p2Fahrplan)) { // Betriebsart 20 Fahrplan senden
 
-            let hasEncoder = false
-            if (checkEncoder)
-                hasEncoder = writeEncoderReset() // Testet ob Encoder vorhanden, Ergebnis in n_Callibot2_x22hasEncoder
+            if (!n_fahrplanBuffer2x2Motoren_gestartet && btf.getaktiviert(buffer, startBit)) { // ma true
+                n_fahrplanBuffer2x2Motoren_gestartet = true
+                btf.zeigeBIN(0, btf.ePlot.bin, 2) // x=2 löschen
 
-            let i = btf.getByte(buffer, btf.eBufferPointer.m0, btf.eBufferOffset.b1_Servo) // Anzahl Durchläufe gesamt
-            if (i == 0)
-                i = 1 // 0=1x 1=1x 2=2x 3=3x ...
+                let hasEncoder = false
+                if (checkEncoder)
+                    hasEncoder = writeEncoderReset() // Testet ob Encoder vorhanden, Ergebnis in n_Callibot2_x22hasEncoder
 
-            for (i; i > 0; i--) {
+                let i = btf.getByte(buffer, btf.eBufferPointer.m0, btf.eBufferOffset.b1_Servo) // Anzahl Durchläufe gesamt
+                if (i == 0)
+                    i = 1 // 0=1x 1=1x 2=2x 3=3x ...
 
-                for (let iBufferPointer = btf.eBufferPointer.ma; iBufferPointer < 19; iBufferPointer += 6) { // 7ab, 13cd
+                for (i; i > 0; i--) {
 
-                    btf.zeigeBINx234Fahrplan2x2Motoren(buffer, iBufferPointer) // anzeigen im 5x5 Display
+                    for (let iBufferPointer = btf.eBufferPointer.ma; iBufferPointer < 19; iBufferPointer += 6) { // 7ab, 13cd
 
-                    let j = btf.getByte(buffer, iBufferPointer, btf.eBufferOffset.b1_Servo) // Anzahl Durchläufe einzeln
-                    if (j == 0)
-                        j = 1
+                        btf.zeigeBINx234Fahrplan2x2Motoren(buffer, iBufferPointer) // anzeigen im 5x5 Display
 
-                    for (j; j > 0; j--) {
+                        let j = btf.getByte(buffer, iBufferPointer, btf.eBufferOffset.b1_Servo) // Anzahl Durchläufe einzeln
+                        if (j == 0)
+                            j = 1
 
-                        if (hasEncoder) {
-                            fahre2MotorenEncoder(
-                                btf.getByte(buffer, iBufferPointer, btf.eBufferOffset.b0_Motor),
-                                btf.getByte(buffer, iBufferPointer + 3, btf.eBufferOffset.b0_Motor),
-                                btf.getByte(buffer, iBufferPointer, btf.eBufferOffset.b2_Fahrstrecke),
-                                btf.getByte(buffer, iBufferPointer + 3, btf.eBufferOffset.b2_Fahrstrecke),
-                                btf.getSensor(buffer, iBufferPointer, btf.eSensor.b7Impulse)
-                            )
-                        }
-                        else {
-                            let zehntelsekunden = btf.getByte(buffer, iBufferPointer, btf.eBufferOffset.b2_Fahrstrecke)
+                        for (j; j > 0; j--) {
 
-                            if (btf.getSensor(buffer, iBufferPointer, btf.eSensor.b7Impulse))
-                                zehntelsekunden /= n_EncoderFaktor
+                            if (hasEncoder) {
+                                fahre2MotorenEncoder(
+                                    btf.getByte(buffer, iBufferPointer, btf.eBufferOffset.b0_Motor),
+                                    btf.getByte(buffer, iBufferPointer + 3, btf.eBufferOffset.b0_Motor),
+                                    btf.getByte(buffer, iBufferPointer, btf.eBufferOffset.b2_Fahrstrecke),
+                                    btf.getByte(buffer, iBufferPointer + 3, btf.eBufferOffset.b2_Fahrstrecke),
+                                    btf.getSensor(buffer, iBufferPointer, btf.eSensor.b7Impulse)
+                                )
+                            }
+                            else {
+                                let zehntelsekunden = btf.getByte(buffer, iBufferPointer, btf.eBufferOffset.b2_Fahrstrecke)
 
-                            fahre2MotorenZeit(
-                                btf.getByte(buffer, iBufferPointer, btf.eBufferOffset.b0_Motor),
-                                btf.getByte(buffer, iBufferPointer + 3, btf.eBufferOffset.b0_Motor),
-                                zehntelsekunden,
-                                btf.getSensor(buffer, iBufferPointer, btf.eSensor.b6Abstand),
-                                btf.getAbstand(buffer),
-                                btf.getSensor(buffer, iBufferPointer, btf.eSensor.b5Spur)
-                            )
-                        }
-                    } // for j
-                } // for iBufferPointer
-            } // for i
+                                if (btf.getSensor(buffer, iBufferPointer, btf.eSensor.b7Impulse))
+                                    zehntelsekunden /= n_EncoderFaktor
+
+                                fahre2MotorenZeit(
+                                    btf.getByte(buffer, iBufferPointer, btf.eBufferOffset.b0_Motor),
+                                    btf.getByte(buffer, iBufferPointer + 3, btf.eBufferOffset.b0_Motor),
+                                    zehntelsekunden,
+                                    btf.getSensor(buffer, iBufferPointer, btf.eSensor.b6Abstand),
+                                    btf.getAbstand(buffer),
+                                    btf.getSensor(buffer, iBufferPointer, btf.eSensor.b5Spur)
+                                )
+                            }
+                        } // for j
+                    } // for iBufferPointer
+                } // for i
+            }
+            else if (n_fahrplanBuffer2x2Motoren_gestartet && !btf.getaktiviert(buffer, startBit)) { // m1 false
+                n_fahrplanBuffer2x2Motoren_gestartet = false
+                btf.zeigeBIN(0, btf.ePlot.bin, 2)
+                btf.zeigeBIN(0, btf.ePlot.bin, 3)
+                btf.zeigeBIN(0, btf.ePlot.bin, 4)
+            }
         }
-        else if (n_fahrplanBuffer2x2Motoren_gestartet && !btf.getaktiviert(buffer, startBit)) { // m1 false
-            n_fahrplanBuffer2x2Motoren_gestartet = false
-            btf.zeigeBIN(0, btf.ePlot.bin, 2)
-            btf.zeigeBIN(0, btf.ePlot.bin, 3)
-            btf.zeigeBIN(0, btf.ePlot.bin, 4)
-        }
-
     }
 
 
--- r-fernsteuerung.ts
+++ r-fernsteuerung.ts
@@ -1,15 +1,17 @@
 
 namespace receiver { // r-fernsteuerung.ts
 
+    // ========== group="00 Fernsteuerung mit Joystick (reagiert auf Sensoren)" subcategory="Fernsteuerung"
+
     let n_AbstandStop = false
     let n_SpurStop = false
 
     //% group="00 Fernsteuerung mit Joystick (reagiert auf Sensoren)" subcategory="Fernsteuerung"
-    //% block="Fahren und Lenken mit Joystick aus Datenpaket %buffer M:01ABCD S:0" weight=5
+    //% block="00 Fahren und Lenken mit Joystick (M:01ABCD S:0) aus %buffer " weight=5
     //% buffer.shadow="btf_receivedBuffer19"
     export function fahreJoystick(buffer: Buffer) {
 
-        if (btf.isBetriebsart(buffer, btf.e0Betriebsart.p0Fahren)) {
+        if (btf.isBetriebsart(buffer, btf.e0Betriebsart.p0Fahren)) { // Betriebsart 00 mit Joystick fernsteuern
 
             // Motor M0+Servo M1 (Fahren und Lenken)
             if (btf.getaktiviert(buffer, btf.e3aktiviert.m0)) {
@@ -90,46 +92,48 @@ namespace receiver { // r-fernsteuerung.ts
     let n_fahrplanBuffer5Strecken_gestartet = false
 
     //% group="20 Fahrplan (5 Teilstrecken) empfangen" subcategory="Fernsteuerung"
-    //% block="fahre Strecke 1-5 aus Datenpaket %buffer Start Bit %startBit" weight=4
+    //% block="20 Fahren Strecke 1-5 (MS:1ABCD) aus %buffer • Start Bit %startBit" weight=4
     //% buffer.shadow=btf_receivedBuffer19
     //% startBit.defl=btf.e3aktiviert.m1
     export function fahrplanBuffer5Strecken(buffer: Buffer, startBit: btf.e3aktiviert) {
 
-        if (!n_fahrplanBuffer5Strecken_gestartet && btf.getaktiviert(buffer, startBit)) { // m1 true
-            n_fahrplanBuffer5Strecken_gestartet = true
-            btf.zeigeBIN(0, btf.ePlot.bin, 2)
+        if (btf.isBetriebsart(buffer, btf.e0Betriebsart.p2Fahrplan)) { // Betriebsart 20 Fahrplan senden
 
-            let i = btf.getByte(buffer, btf.eBufferPointer.m0, btf.eBufferOffset.b1_Servo) // Anzahl Durchläufe gesamt in m0-Servo
-            if (i == 0) // 0 wie 1 behandeln = 1 Durchlauf
-                i = 1 // 0=1x 1=1x 2=2x 3=3x ...
+            if (!n_fahrplanBuffer5Strecken_gestartet && btf.getaktiviert(buffer, startBit)) { // m1 true
+                n_fahrplanBuffer5Strecken_gestartet = true
+                btf.zeigeBIN(0, btf.ePlot.bin, 2)
 
-            for (i; i > 0; i--) {
+                let i = btf.getByte(buffer, btf.eBufferPointer.m0, btf.eBufferOffset.b1_Servo) // Anzahl Durchläufe gesamt in m0-Servo
+                if (i == 0) // 0 wie 1 behandeln = 1 Durchlauf
+                    i = 1 // 0=1x 1=1x 2=2x 3=3x ...
 
-                for (let iBufferPointer = btf.eBufferPointer.m1; iBufferPointer < 19; iBufferPointer += 3) { // 4, 7, 10, 13, 16
+                for (i; i > 0; i--) {
 
-                    btf.zeigeBINx234Fahrplan5Strecken(buffer, iBufferPointer) // anzeigen im 5x5 Display
+                    for (let iBufferPointer = btf.eBufferPointer.m1; iBufferPointer < 19; iBufferPointer += 3) { // 4, 7, 10, 13, 16
 
-                    // fahreStrecke testet Gültigkeit der Parameter
-                    // fahreStrecke wertet auch Encoder, Abstand- und Spur- Sensoren aus
-                    fahreStrecke(
-                        btf.getByte(buffer, iBufferPointer, btf.eBufferOffset.b0_Motor),
-                        btf.getByte(buffer, iBufferPointer, btf.eBufferOffset.b1_Servo),
-                        btf.getByte(buffer, iBufferPointer, btf.eBufferOffset.b2_Fahrstrecke),
-                        btf.getSensor(buffer, iBufferPointer, btf.eSensor.b6Abstand),
-                        btf.getAbstand(buffer),
-                        btf.getSensor(buffer, iBufferPointer, btf.eSensor.b5Spur),
-                        btf.getSensor(buffer, iBufferPointer, btf.eSensor.b7Impulse)
-                    )
-                } // for iBufferPointer
+                        btf.zeigeBINx234Fahrplan5Strecken(buffer, iBufferPointer) // anzeigen im 5x5 Display
+
+                        // fahreStrecke testet Gültigkeit der Parameter
+                        // fahreStrecke wertet auch Encoder, Abstand- und Spur- Sensoren aus
+                        fahreStrecke(
+                            btf.getByte(buffer, iBufferPointer, btf.eBufferOffset.b0_Motor),
+                            btf.getByte(buffer, iBufferPointer, btf.eBufferOffset.b1_Servo),
+                            btf.getByte(buffer, iBufferPointer, btf.eBufferOffset.b2_Fahrstrecke),
+                            btf.getSensor(buffer, iBufferPointer, btf.eSensor.b6Abstand),
+                            btf.getAbstand(buffer),
+                            btf.getSensor(buffer, iBufferPointer, btf.eSensor.b5Spur),
+                            btf.getSensor(buffer, iBufferPointer, btf.eSensor.b7Impulse)
+                        )
+                    } // for iBufferPointer
+                }
             }
-        }
-        else if (n_fahrplanBuffer5Strecken_gestartet && !btf.getaktiviert(buffer, startBit)) { // m1 false
-            n_fahrplanBuffer5Strecken_gestartet = false
-            btf.zeigeBIN(0, btf.ePlot.bin, 2)
-            btf.zeigeBIN(0, btf.ePlot.bin, 3)
-            btf.zeigeBIN(0, btf.ePlot.bin, 4)
-        }
-
+            else if (n_fahrplanBuffer5Strecken_gestartet && !btf.getaktiviert(buffer, startBit)) { // m1 false
+                n_fahrplanBuffer5Strecken_gestartet = false
+                btf.zeigeBIN(0, btf.ePlot.bin, 2)
+                btf.zeigeBIN(0, btf.ePlot.bin, 3)
+                btf.zeigeBIN(0, btf.ePlot.bin, 4)
+            }
+        } // 0x20 Fahrplan
     }
 
 } // r-fernsteuerung.ts
