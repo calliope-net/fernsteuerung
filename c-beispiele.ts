d2fe57ea12e7ef2e04f668eadd0b2b955ca9426b
calliope-net <141074597+calliope-net@users.noreply.github.com>
2024-08-15 12:24:56 +0200

    Aktualisiere main.blocks, c-beispiele.ts

--- c-beispiele.ts
+++ c-beispiele.ts
@@ -10,8 +10,16 @@ namespace cb2 { // c-beispiele.ts
     let m_lenken: number
     let m_inSpur = false
 
-
-    export function eventSpurfolger(gestartet: boolean, links_hell: boolean, rechts_hell: boolean, motor128: number, langsamfahren: number, servo16: number, lenkenProzent: number, stop: boolean, abstand: number, index = 0) {
+    //% group="Spurfolger Ereignis" subcategory=Beispiele
+    //% block="Spurfolger: Calli:bot | gestartet %gestartet <links_hell> %links_hell <rechts_hell> %rechts_hell <abstand_Stop> %abstand_Stop Fahren (1↓128↑255) %motor128 langsam Fahren %motorLenken Lenken (1↖16↗31) %servo16 lenkender Motor \\% %lenkenProzent Abstand Sensor %abstandSensor bei Abstand < (cm) %abstand" weight=6
+    //% motor128.min=1 motor128.max=255 motor128.defl=192
+    //% servo16.min=1 servo16.max=31 servo16.defl=31
+    //% langsamfahren.min=1 langsamfahren.max=255 langsamfahren.defl=160
+    //% lenkenProzent.min=10 lenkenProzent.max=90 lenkenProzent.defl=0
+    //% repeat.shadow="toggleYesNo" repeat.defl=1
+    //% stop.shadow=toggleOnOff stop.defl=1
+    //% abstand.min=10 abstand.max=50 abstand.defl=20
+    export function eventSpurfolger(gestartet: boolean, links_hell: boolean, rechts_hell: boolean, abstand_Stop: boolean, motor128: number, motorLenken: number, servo16: number, lenkenProzent: number, abstandSensor: boolean, abstand: number, index = 0) {
         if (gestartet) {
 
             btf.reset_timer()
@@ -22,7 +30,7 @@ namespace cb2 { // c-beispiele.ts
                 writecb2RgbLeds(Colors.Off, false) // alle 4 aus
             }
 
-            if (stop) {
+            if (abstandSensor && abstand > 0 && abstand_Stop) {
                 writeMotorenStop()
                 writecb2RgbLed(eRgbLed.lh, Colors.Red, true)
                 basic.pause(Math.randomRange(500, 5000)) // 0.5 .. 5 Sekunden warten bis es wieder los fährt
@@ -31,24 +39,24 @@ namespace cb2 { // c-beispiele.ts
 
                 let lenken = Math.abs(servo16 - 16)  // 16-16=0 / 1-16=15 / 31-16=15
 
-               // readInputs(i2cSpur) // liest Spursensor ein
+                // readInputs(i2cSpur) // liest Spursensor ein
 
                 if (!links_hell && !rechts_hell) { // dunkel dunkel
                     writeMotor128Servo16(motor128, 16) // nicht lenken
                     m_inSpur = true
                 }
                 else if (!links_hell && rechts_hell) { // dunkel hell
-                    writeMotor128Servo16(langsamfahren, 16 - lenken, lenkenProzent) // links lenken <16 = 1
+                    writeMotor128Servo16(motorLenken, 16 - lenken, lenkenProzent) // links lenken <16 = 1
                     if (m_inSpur)
                         m_lenken = 16 - lenken
                 }
                 else if (links_hell && !rechts_hell) { // hell dunkel
-                    writeMotor128Servo16(langsamfahren, 16 + lenken, lenkenProzent) // rechts lenken >16 = 31
+                    writeMotor128Servo16(motorLenken, 16 + lenken, lenkenProzent) // rechts lenken >16 = 31
                     if (m_inSpur)
                         m_lenken = 16 + lenken
                 }
                 else if (m_lenken) { // hell hell
-                    writeMotor128Servo16(langsamfahren, m_lenken, lenkenProzent) // lenken wie zuletzt gespeichert
+                    writeMotor128Servo16(motorLenken, m_lenken, lenkenProzent) // lenken wie zuletzt gespeichert
                     m_inSpur = false // hell hell
                 }
                 else { // hell hell
@@ -56,7 +64,7 @@ namespace cb2 { // c-beispiele.ts
                     m_inSpur = false // hell hell
                 }
 
-                writecb2RgbLed(eRgbLed.lh, Colors.Yellow, stop)
+                writecb2RgbLed(eRgbLed.lh, Colors.Yellow, abstandSensor)
             }
             a_eventSpurfolger_gestartet[index] = true
         }
