4e0459f764ced8eeaae51d4dc7d9af726177296c
calliope-net <141074597+calliope-net@users.noreply.github.com>
2024-08-22 10:16:35 +0200

    Aktualisiere main.blocks, r-pins.ts, r-spursensor.ts, r-sensorevents.ts

--- r-pins.ts
+++ r-pins.ts
@@ -3,10 +3,93 @@ namespace receiver { // r-pins.ts
 
     // PINs sind in r-receiver.ts definiert
 
-    // ========== group="Digital Pins (vom gewählten Modell)" subcategory="Pins, Sensoren"
+
+
+    // ========== group="Spursensor" subcategory="Pins"
+
+    //   export enum eDH { dunkel = 0, hell = 1 } // 0 ist schwarz
+
+    //% group="Spursensor (vom gewählten Modell)" subcategory="Pins"
+    //% block="Spursensor Pin links %hell" weight=6
+    export function pinSpurlinks(hell: eDH) {
+        if (a_PinSpurlinks.length > n_Hardware)
+            return pins.digitalReadPin(a_PinSpurlinks[n_Hardware]) == hell // 0 ist schwarz
+        else
+            return false
+    }
+
+    //% group="Spursensor (vom gewählten Modell)" subcategory="Pins"
+    //% block="Spursensor Pin rechts %hell" weight=5
+    export function pinSpurrechts(hell: eDH) {
+        if (a_PinSpurrechts.length > n_Hardware)
+            return pins.digitalReadPin(a_PinSpurrechts[n_Hardware]) == hell // 0 ist schwarz
+        else
+            return false
+    }
+
+
+    //% group="Spursensor (vom gewählten Modell)" subcategory="Pins"
+    //% block="Spursensor Pin links %l und rechts %r" weight=4
+    export function readSpursensor(l: eDH, r: eDH) {
+        return pinSpurlinks(l) && pinSpurrechts(r)
+    }
+
+
+    // ========== EVENT HANDLER === sichtbarer Event-Block
+    export let onSpurPinEventHandler: (links: boolean, rechts: boolean) => void
+    //  let onSpurStopEventHandler: (links: boolean, rechts: boolean, abstand_Stop: boolean) => void
+
+    //% group="Spursensor (vom gewählten Modell)" subcategory="Pins"
+    //% block="wenn Spur Sensor Pin Ereignis" weight=2
+    //% draggableParameters=reporter
+    export function onSpurPinEvent(cb: (links_hell: boolean, rechts_hell: boolean) => void) {
+        onSpurPinEventHandler = cb
+    }
+
+
+
+
+
+
+    // ========== group="Ultraschall (vom gewählten Modell)" subcategory="Pins"
+
+    //% group="Ultraschall (vom gewählten Modell)" subcategory="Pins"
+    //% block="Abstand Sensor angeschlossen" weight=7
+    export function selectAbstandSensorConnected() {
+        if (n_Hardware == eHardware.v3)
+            return n_QwiicUltrasonicConnected
+        else if (n_Hardware == eHardware.car4)
+            return true
+        else
+            return false
+    }
+
+    //% group="Ultraschall (vom gewählten Modell)" subcategory="Pins"
+    //% block="Abstand cm • einlesen %read" weight=6
+    //% read.shadow=toggleYesNo
+    export function selectAbstand(read: boolean) {
+        if (n_Hardware == eHardware.v3)
+            return getQwiicUltrasonic(read) // in r-qwiic.ts i2c einlesen
+        else if (n_Hardware == eHardware.car4)
+            return pinGroveUltraschall_cm() // in r-advanced.ts
+        else
+            return 0
+    }
+
+    //% blockId=receiver_getAbstand blockHidden=true
+    //% block="%buffer Abstand in cm" weight=3
+    //% buffer.shadow="btf_receivedBuffer19"
+    export function receiver_getAbstand(buffer: Buffer) {
+        return btf.getAbstand(buffer)
+    }
+
+
+
+
+    // ========== group="Digital Pins (vom gewählten Modell)" subcategory="Pins"
     // Relais auf der Leiterplatte schaltet 9V Akku für eigene Stromversorgung an VM+
 
-    //% group="Digital Pins (vom gewählten Modell)" subcategory="Pins, Sensoren"
+    //% group="Digital Pins (vom gewählten Modell)" subcategory="Pins"
     //% block="Stromversorgung 9V %pON" weight=8
     //% pON.shadow="toggleOnOff"
     export function pinRelay(pON: boolean) {
@@ -14,7 +97,7 @@ namespace receiver { // r-pins.ts
             pins.digitalWritePin(a_PinRelay[n_Hardware], pON ? 1 : 0)
     }
 
-    //% group="Digital Pins (vom gewählten Modell)" subcategory="Pins, Sensoren"
+    //% group="Digital Pins (vom gewählten Modell)" subcategory="Pins"
     //% block="Licht %pON" weight=7
     //% pON.shadow="toggleOnOff"
     export function pinLicht(pON: boolean) {
@@ -33,77 +116,14 @@ namespace receiver { // r-pins.ts
         C17 = DigitalPin.C17
     }
 
-    //% group="Digital Pins (vom gewählten Modell)" subcategory="Pins, Sensoren"
+    //% group="Digital Pins (vom gewählten Modell)" subcategory="Pins"
     //% block="Digital Pin %pin %pON" weight=6
     //% pON.shadow="toggleOnOff"
     export function digitalWritePin(pin: eDigitalPins, pON: boolean) {
         pins.digitalWritePin(<number>pin, pON ? 0 : 1)
     }
 
-  
-
-    // ========== group="Spursensor" subcategory="Pins, Sensoren"
-/* 
-    export enum eDH { dunkel = 0, hell = 1 } // 0 ist schwarz
-
-    //% group="Spursensor (vom gewählten Modell)" subcategory="Pins, Sensoren"
-    //% block="Spursensor links %hell" weight=6
-    export function pinSpurlinks(hell: eDH) {
-        if (a_PinSpurlinks.length > n_Hardware)
-            return pins.digitalReadPin(a_PinSpurlinks[n_Hardware]) == hell // 0 ist schwarz
-        else
-            return false
-    }
-
-    //% group="Spursensor (vom gewählten Modell)" subcategory="Pins, Sensoren"
-    //% block="Spursensor rechts %hell" weight=5
-    export function pinSpurrechts(hell: eDH) {
-        if (a_PinSpurrechts.length > n_Hardware)
-            return pins.digitalReadPin(a_PinSpurrechts[n_Hardware]) == hell // 0 ist schwarz
-        else
-            return false
-    }
- */
-
-    //% group="Spursensor (vom gewählten Modell)" subcategory="Pins, Sensoren"
-    //% block="Spursensor links %l und rechts %r" weight=4
-    //export function readSpursensor(l: eDH, r: eDH) {
-    //    return pinSpurlinks(l) && pinSpurrechts(r)
-    //}
 
 
-
-
-    // ========== group="Ultraschall (vom gewählten Modell)" subcategory="Pins, Sensoren"
-
-    //% group="Ultraschall (vom gewählten Modell)" subcategory="Pins, Sensoren"
-    //% block="Abstand Sensor angeschlossen" weight=7
-    export function selectAbstandSensorConnected() {
-        if (n_Hardware == eHardware.v3)
-            return n_QwiicUltrasonicConnected
-        else if (n_Hardware == eHardware.car4)
-            return true
-        else
-            return false
-    }
-
-    //% group="Ultraschall (vom gewählten Modell)" subcategory="Pins, Sensoren"
-    //% block="Abstand cm • einlesen %read" weight=6
-    //% read.shadow=toggleYesNo
-    export function selectAbstand(read: boolean) {
-        if (n_Hardware == eHardware.v3)
-            return getQwiicUltrasonic(read) // in r-qwiic.ts i2c einlesen
-        else if (n_Hardware == eHardware.car4)
-            return pinGroveUltraschall_cm() // in r-advanced.ts
-        else
-            return 0
-    }
-
-    //% blockId=receiver_getAbstand blockHidden=true
-    //% block="%buffer Abstand in cm" weight=3
-    //% buffer.shadow="btf_receivedBuffer19"
-    export function receiver_getAbstand(buffer: Buffer) {
-        return btf.getAbstand(buffer)
-    }
 
 }
--- r-spursensor.ts
+++ r-spursensor.ts
@@ -1,76 +1,6 @@
 
 namespace receiver { // r-spursensor.ts
 
-    let n_SpursensorEventsRegistered = false
-    //let n_inEvent = 0
-    export let n_SpurLinksHell = false // hell=true
-    export let n_SpurRechtsHell = false
-    const c_pulseDuration = 60000 // µs 50 ms
-
-    //% group="Spursensor (vom gewählten Modell)" subcategory="Pins, Sensoren"
-    //% block="Spursensor Pin Ereignisse registrieren (beim Start)" weight=8
-    export function spursensorRegisterEvents() {
-        if (!n_SpursensorEventsRegistered) {
-            // n_Spursensor = (pins.digitalReadPin(a_PinSpurlinks[n_Hardware]) << 1) | pins.digitalReadPin(a_PinSpurrechts[n_Hardware])
-            n_SpurLinksHell = pins.digitalReadPin(a_PinSpurlinks[n_Hardware]) == 1
-            n_SpurRechtsHell = pins.digitalReadPin(a_PinSpurrechts[n_Hardware]) == 1
-
-            // PulseValue.Low
-            // ↑high, ↓low, Event niedrig bei l->h loslassen
-            // Zeit wie lange es low ↓↑ war in µs
-
-            pins.onPulsed(a_PinSpurlinks[n_Hardware], PulseValue.Low, function () {
-                // links hell
-                if (pins.pulseDuration() > c_pulseDuration) { // 10ms
-                    n_SpurLinksHell = true
-                    // raiseSpurEvent()
-                    if (onSpurPinEventHandler)
-                        onSpurPinEventHandler(n_SpurLinksHell, n_SpurRechtsHell)
-                    // if (onSpurStopEventHandler)
-                    //     onSpurStopEventHandler(n_SpurLinksHell, n_SpurRechtsHell, n_AbstandStop)
-
-                }
-            })
-            pins.onPulsed(a_PinSpurlinks[n_Hardware], PulseValue.High, function () {
-                // links dunkel
-                if (pins.pulseDuration() > c_pulseDuration) { // 10ms
-                    n_SpurLinksHell = false
-                    // raiseSpurEvent()
-                    if (onSpurPinEventHandler)
-                        onSpurPinEventHandler(n_SpurLinksHell, n_SpurRechtsHell)
-                    // if (onSpurStopEventHandler)
-                    //     onSpurStopEventHandler(n_SpurLinksHell, n_SpurRechtsHell, n_AbstandStop)
-                }
-            })
-
-            pins.onPulsed(a_PinSpurrechts[n_Hardware], PulseValue.Low, function () {
-                // rechts hell
-                if (pins.pulseDuration() > c_pulseDuration) { // 10ms
-                    n_SpurRechtsHell = true
-                    // raiseSpurEvent()
-                    if (onSpurPinEventHandler)
-                        onSpurPinEventHandler(n_SpurLinksHell, n_SpurRechtsHell)
-                    // if (onSpurStopEventHandler)
-                    //     onSpurStopEventHandler(n_SpurLinksHell, n_SpurRechtsHell, n_AbstandStop)
-                }
-            })
-            pins.onPulsed(a_PinSpurrechts[n_Hardware], PulseValue.High, function () {
-                // rechts dunkel
-                if (pins.pulseDuration() > c_pulseDuration) { // 10ms
-                    n_SpurRechtsHell = false
-                    // raiseSpurEvent()
-                    if (onSpurPinEventHandler)
-                        onSpurPinEventHandler(n_SpurLinksHell, n_SpurRechtsHell)
-                    // if (onSpurStopEventHandler)
-                    //     onSpurStopEventHandler(n_SpurLinksHell, n_SpurRechtsHell, n_AbstandStop)
-                }
-            })
-
-            // danach darf kein pins.digitalReadPin() stehen, das deaktiviert die Ereignisse wieder, davor ist möglich
-            //n_inEvent = 0
-            n_SpursensorEventsRegistered = true
-        }
-    }
     /* 
         function raiseSpurEvent() {
             if (onSpurEventHandler) {
@@ -91,52 +21,12 @@ namespace receiver { // r-spursensor.ts
         }
     */
 
-    export enum eDH { hell = 1, dunkel = 0 }
-
-    //% group="Spursensor (vom gewählten Modell)" subcategory="Pins, Sensoren"
-    //% block="Spursensor links %l" weight=6
-    export function getSpurLinks(l: eDH) {
-        return (l == eDH.hell) ? n_SpurLinksHell : !n_SpurLinksHell
-    }
-
-    //% group="Spursensor (vom gewählten Modell)" subcategory="Pins, Sensoren"
-    //% block="Spursensor rechts %r" weight=5
-    export function getSpurRechts(r: eDH) {
-        return (r == eDH.hell) ? n_SpurRechtsHell : !n_SpurRechtsHell
-    }
-
-    //% group="Spursensor (vom gewählten Modell)" subcategory="Pins, Sensoren"
-    //% block="Spursensor links %rechts rechts • dunkel %hell hell" weight=4
-    //% rechts.shadow=toggleYesNo
-    //% hell.shadow=toggleYesNo
-    /* export function getSpursensor(rechts: boolean, hell: boolean) {
-        let bm = rechts ? 0b01 : 0b10
-        return (n_Spursensor & bm) == (hell ? bm : 0)
-    } */
-
-    //% group="Spursensor (vom gewählten Modell)" subcategory="Pins, Sensoren"
-    //% block="Spursensoren links %l und rechts %r" weight=3
-    export function getSpursensor(l: eDH, r: eDH) {
-        return getSpurLinks(l) && getSpurRechts(r)
-    }
-
     /* export enum eINPUTS {
         //% block="Spursensor rechts hell"
         spr = 0b00000001,
         //% block="Spursensor links hell"
         spl = 0b00000010
     } */
-
-    // ========== EVENT HANDLER === sichtbarer Event-Block
-    let onSpurPinEventHandler: (links: boolean, rechts: boolean) => void
-    //  let onSpurStopEventHandler: (links: boolean, rechts: boolean, abstand_Stop: boolean) => void
-
-    //% group="Spursensor (vom gewählten Modell)" subcategory="Pins, Sensoren"
-    //% block="wenn Spur Sensor Pin Ereignis" weight=2
-    //% draggableParameters=reporter
-    export function onSpurPinEvent(cb: (links_hell: boolean, rechts_hell: boolean) => void) {
-        onSpurPinEventHandler = cb
-    }
 
     //% group="Spursensor (vom gewählten Modell)" subcategory="Pins, Sensoren"
     //% block="wenn Sensor geändert" weight=1
